36c7cbb6748840f376474c6c4a9e64921c69d6df
Katarzyna Kensy <zbierek@gmail.com>
2018-08-19 19:13:59 +0200

    Add all files

--- server/server.js
+++ server/server.js
@@ -70,7 +70,8 @@ app.use(compression());
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
 app.use(Express.static(path.resolve(__dirname, '../dist/client')));
-app.use('/api', posts);
+app.use('/api', lanes);
+app.use('/api', notes);
 
 // Render Initial HTML
 const renderFullPage = (html, initialState) => {
